938e62688d129bfc9c963c4df2e816aff6ad8a39
AlexSkorupsky <alex.skorupskyy@gmail.com>
2019-03-13 05:13:04 +0200

    updated server & file system, added view ejs templates

--- static/scripts/errorMenuScript.js
+++ static/scripts/errorMenuScript.js
@@ -1,11 +1,10 @@
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
-    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
   }
   
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("main").style.marginLeft= "0";
-    document.body.style.backgroundColor = "white";
+    document.body.style.backgroundColor = "black";
   }
